bcd7dc80b876960077d3fda30876fe979168fd7e
RangerMauve <RangerMauve@hotmail.com>
2014-06-23 14:41:21 -0400

    Initial working commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = {};
+var xhr = require("xhr");
+var Promise = require("promise");
+
+/**
+ * XHR wrapper, see https://github.com/matthewp/xhr for API
+ * @param options URL or Object for use in XHR
+ * @returns {Promise} resolves to response data, rejects to XhrError instance
+ */
+module.exports = function (options) {
+	return new Promise(function (resolve, reject) {
+		xhr(options, resolve, reject)
+	});
+};
